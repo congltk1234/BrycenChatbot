ff9fd18bc448554f0cab08fd8bf342c11b5280f6
STC1702 <stc17022002@gmail.com>
2023-08-05 20:51:01 +0700

    Control Drawler with button and 2 drawler in single page

--- lib/widget/menu.dart
+++ lib/widget/menu.dart
@@ -1,4 +1,5 @@
 import 'package:brycen_chatbot/firebase_options.dart';
+import 'package:brycen_chatbot/widget/chatTitle.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -18,7 +19,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
       title: appTitle,
       home: MyHomePage(title: appTitle),
     );
@@ -36,8 +38,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
-
-  var _widgetOptions;
+  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  late List<ChatTitle> _widgetOptions;
 
   void _onItemTapped(int index) {
     setState(() {
@@ -50,14 +52,42 @@ class _MyHomePageState extends State<MyHomePage> {
     menuList();
 
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
-      body: Center(
-        child: Text(_widgetOptions[_selectedIndex]['Username']),
+      key: scaffoldKey,
+      // appBar: AppBar(title: Text(widget.title)),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Text('Drawler demo'),
+              TextButton(
+                  child: Text('Chat'),
+                  onPressed: () {
+                    scaffoldKey.currentState!.openDrawer();
+                  }),
+              TextButton(
+                  child: Text('Summarize'),
+                  onPressed: () {
+                    // scaffoldKey.currentState!.openDrawer();
+                    scaffoldKey.currentState!.openEndDrawer();
+                  }),
+            ],
+          ),
+        ),
       ),
       drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: _buildTiles(),
+        child: SafeArea(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: _buildTiles(),
+          ),
+        ),
+      ),
+      endDrawer: Drawer(
+        child: SafeArea(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: _buildTiles(),
+          ),
         ),
       ),
     );
@@ -66,14 +96,15 @@ class _MyHomePageState extends State<MyHomePage> {
   void menuList() async {
     final users = await FirebaseFirestore.instance.collection("users").get();
 
-    List widgetList = [];
+    List<ChatTitle> widgetList = [];
 
     for (var element in users.docs) {
       // final option = Text(element.data()['Username']);
-      widgetList.add({
-        'Username': element.data()['Username'],
-        'APIkey': element.data()['APIkey']
-      });
+      widgetList.add(ChatTitle(
+          id: element.id,
+          title: element.data()['Username'],
+          // chatDate: element.data()['createdAt'],
+          apiKey: element.data()['APIkey']));
     }
 
     setState(() {
@@ -93,16 +124,17 @@ class _MyHomePageState extends State<MyHomePage> {
 
     for (var element in _widgetOptions) {
       tiles.add(ListTile(
-        title: Text(element['Username']),
+        title: Text(element.title),
         onTap: () {
           Navigator.push(
             context,
             MaterialPageRoute(
               builder: (context) => MenuItemPage(
-                apiKey: element['APIkey'],
+                chatTitle: element,
               ),
             ),
           );
+          // Navigator.pop(context);
         },
       ));
     }
@@ -112,19 +144,19 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 class MenuItemPage extends StatelessWidget {
-  final String apiKey;
+  final ChatTitle chatTitle;
 
-  const MenuItemPage({super.key, required this.apiKey});
+  const MenuItemPage({super.key, required this.chatTitle});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Another Page"),
+        title: Text("${chatTitle.title}"),
       ),
       body: Center(
           child: Text(
-        'Your API is $apiKey',
+        'Your API is ${chatTitle.apiKey}',
         textAlign: TextAlign.center,
         overflow: TextOverflow.ellipsis,
         style: TextStyle(fontWeight: FontWeight.bold),
